01401f158928785cf158c06d63d440269be176eb
ophirbushi <ophirbushi@gmail.com>
2015-11-07 14:36:51 +0200

    Added toggleCourses function.

--- Partyturot.js
+++ Partyturot.js
@@ -1,12 +1,38 @@
 Marks = new Mongo.Collection("marks");
-Assignments= new Mongo.Collection("assignments");
+Assignments = new Mongo.Collection("assignments");
 Attendance = new Mongo.Collection("attendance");
 
+function toggleCourses() {
+    $(".course").each(function () {
+        if ($(this).hasClass("active_course")) {
+            $(this).removeClass("active_course");
+            $(this).addClass("non_active_course");
+        }
+        else {
+            $(this).addClass("active_course");
+            $(this).removeClass("non_active_course");
+        }
+    });
+}
 
 
 if (Meteor.isClient) {
 
+    Template.body.helpers({
+        "courses": function () {
+            return [{ "isActive": "active_course", "text": "first course" }, { "isActive": "non_active_course" , "text": "second course"}];
+        }
+    });
 
+    Template.body.events({
+        
+    });
+
+    Template.course.helpers({
+        "isActive": function () {
+            return this.isActive;
+        }
+    });
 }
 
 if (Meteor.isServer) {
@@ -14,3 +40,5 @@ if (Meteor.isServer) {
         // code to run on server at startup
     });
 }
+
+
